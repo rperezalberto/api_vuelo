94d2e78004dbc46bb342059c1b824e8d3872939b
Robin Perez <rperezalberto@gmail.com>
2022-06-17 22:15:48 +0200

    api python

---
+++ src/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+from config import config
+
+app = Flask(__name__)
+conexion = MySQL(app)
+
+@app.route('/vuelos')
+def listar_vuelos():
+    try:
+        db_cursor = conexion.connection
+    except Exception as ex:
+        return ex
+
+
+def pagina_not_found(error):
+    return "<h1>La pagina que intentas buscar no existe...</h1>"
+
+if __name__ == '__main__':
+    app.config.from_object(config['development'])
+    app.register_error_handler(404, pagina_not_found)
+    app.run()
+
---
+++ src/config.py
@@ -0,0 +1,13 @@
+from distutils.debug import DEBUG
+
+
+class DevelopmentConfig():
+    DEBUG = True
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'admin'
+    MYSQL_DB = 'db_vuelos'
+
+config = {
+    'development': DevelopmentConfig
+}
