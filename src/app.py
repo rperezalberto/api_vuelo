6d1b5b0b3c62cfc0e6144d452fc860afc3d2c60c
Robin Perez <35049749+rperezalberto@users.noreply.github.com>
2022-06-19 14:43:47 -0400

    ver vuelos

--- src/app.py
+++ src/app.py
@@ -1,19 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_mysqldb import MySQL
+
 
 from config import config
 
 app = Flask(__name__)
-conexion = MySQL(app)
+mysql = MySQL(app)
 
-@app.route('/vuelos')
+
+@app.route('/')
 def listar_vuelos():
-    # return ("Hola mundo")
     try:
-       cursor = conexion.connection.cursor()
-       cursor.execute(sql)
-       datos = cursor.fetchall()
-       return "datos"
+        cursor = mysql.connection.cursor()
+        sql = "SELECT origen.aeropuerto_origen, origen.ciudad_orgien, destino.aeropuerto_destino, destino.ciudad_destino, compania.nombre_compania, compania.direccion, compania.telefono_compania, vuelo.modeloAvion, vuelo.capacidad, vuelo.numeroVuelo FROM `vuelo` INNER JOIN compania on vuelo._id_compania = compania._id_compania INNER JOIN itinerario on vuelo._id_itinerario = itinerario._id_itinerario INNER JOIN origen on itinerario._id_origen = origen._id_origen INNER JOIN destino on itinerario._id_destino = destino._id_destino"
+        cursor.execute(sql)
+        data = cursor.fetchall()
+
+        vuelos = []
+
+        for fila in data:
+            
+            vuelo = {"aeropuerto_origen": fila[0], "ciudad_orgien":fila[1], "aeropuerto_destino": fila[2], "ciudad_destino":fila[3], "nombre_compania":fila[4], "direccion_compnia":fila[5], "telefono_compania":fila[6], "modeloAvion": fila[7], "capacidad":fila[8], "numeroVuelo":fila[9]}
+            vuelos.append(vuelo)
+
+        return jsonify({"vuelos": vuelos, "mensaje":"Listado de vuelos"})
+
 
     except Exception as ex:
         return ex
@@ -25,5 +36,6 @@ def pagina_not_found(error):
 if __name__ == '__main__':
     app.config.from_object(config['development'])
     app.register_error_handler(404, pagina_not_found)
-    app.run()
+    app.run(debug=True)
+
 
